98148c5c055bc913439b9950847371b02a977426
isb.redes <isbredes@172-1-157-232.lightspeed.irvnca.sbcglobal.net>
2019-11-17 21:10:58 -0600

    prueba de tkinter

--- sample.py
+++ sample.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 from tkinter import *
-root=Tk()
+root = Tk()
+label = Label( root, text="esto es un label" )
+button = Button( root, text="texto del boton" )
+label.pack()
+button.pack()
 root.mainloop()
-
